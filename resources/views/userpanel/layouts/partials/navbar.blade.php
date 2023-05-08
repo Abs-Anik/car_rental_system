f56ba630a59037154d0fee1567dcf6fe3a16b844
Md. Abu Bakkar Siddik <abubakkarsiddik.cse@gmail.com>
2023-05-08 10:30:05 +0600

    user panel setup

--- resources/views/userpanel/layouts/partials/navbar.blade.php
+++ resources/views/userpanel/layouts/partials/navbar.blade.php
@@ -82,7 +82,7 @@
             <a class="dropdown-item" href="{{ route('logout') }}"
             onclick="event.preventDefault();
                           document.getElementById('logout-form').submit();">
-                          <i class="ti-power-off text-primary"></i>
+                          <i class="ti-power-off text-primary"> </i>
             {{ __('Logout') }}
         </a>
 
