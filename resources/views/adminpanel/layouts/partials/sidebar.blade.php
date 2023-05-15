ee5581f38ea455660609adf05b2690aa3bb691dd
Md. Abu Bakkar Siddik <abubakkarsiddik.cse@gmail.com>
2023-05-15 16:52:20 +0600

    add logo

--- resources/views/adminpanel/layouts/partials/sidebar.blade.php
+++ resources/views/adminpanel/layouts/partials/sidebar.blade.php
@@ -8,8 +8,8 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <img src="{{asset('public/adminpanel/assets/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">hireME!</span>
+      <img src="{{asset('public/adminpanel/assets/dist/img/logo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">hireCAR!</span>
     </a>
 
     <!-- Sidebar -->
