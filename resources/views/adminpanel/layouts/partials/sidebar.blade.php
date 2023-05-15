b3830560f19e78cf14ee106ea88e572ecf6f20d1
Md. Abu Bakkar Siddik <abubakkarsiddik.cse@gmail.com>
2023-05-15 16:40:59 +0600

    menu setup

--- resources/views/adminpanel/layouts/partials/sidebar.blade.php
+++ resources/views/adminpanel/layouts/partials/sidebar.blade.php
@@ -1,8 +1,15 @@
+@php
+    $user = Auth::guard('web')->user();
+    $prefix = Request::route()->getPrefix();
+    $route = Route::current()->getName();
+@endphp
+
+
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
       <img src="{{asset('public/adminpanel/assets/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <span class="brand-text font-weight-light">hireME!</span>
     </a>
 
     <!-- Sidebar -->
@@ -15,15 +22,15 @@
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
           <li class="nav-item menu-open">
-            <a href="#" class="nav-link active">
+            <a href="{{route('admin.dashboard')}}" class="nav-link {{ $route == 'admin.dashboard' ? 'active' : '' }}">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
                 Dashboard
               </p>
             </a>
           </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
+          <li class="nav-item {{(($route == 'admin.rolePermission.create') ? "menu-open" :(($route == 'admin.rolePermission.index') ? "menu-open" : ""))}}">
+            <a href="#" class="nav-link {{(($route == 'admin.rolePermission.create') ? "active" :(($route == 'admin.rolePermission.index') ? "active" : ""))}}">
               <i class="nav-icon fas fa-users"></i>
               <p>
                 User Management
@@ -32,21 +39,21 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="{{ route('admin.rolePermission.create') }}" class="nav-link">
+                <a href="{{ route('admin.rolePermission.create') }}" class="nav-link {{ $route == 'admin.rolePermission.create' ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
                 <p>Assign Role Permission</p>
                 </a>
             </li>
 
             <li class="nav-item">
-                <a href="{{ route('admin.rolePermission.index') }}" class="nav-link">
+                <a href="{{ route('admin.rolePermission.index') }}" class="nav-link {{ $route == 'admin.rolePermission.index' ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
                 <p>Role Permission List</p>
                 </a>
             </li>
             </ul>
           </li>
-          <li class="nav-item">
+          {{-- <li class="nav-item">
             <a href="pages/widgets.html" class="nav-link">
               <i class="nav-icon fas fa-th"></i>
               <p>
@@ -647,7 +654,7 @@
               <i class="nav-icon far fa-circle text-info"></i>
               <p>Informational</p>
             </a>
-          </li>
+          </li> --}}
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
